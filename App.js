9c4cd73d53f447bdc5cac2902da72e4764b31172
Danish Qureshi <85394659+Danishqureshi8817@users.noreply.github.com>
2023-08-06 22:42:25 +0530

    hh

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Navigation from './src/navigation/index'
+const App = () => {
+  return (
+   <Navigation/>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({})
