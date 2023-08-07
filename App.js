71773e9c157e81bcb8c7899f4cca895ea96e9828
Danish Qureshi <85394659+Danishqureshi8817@users.noreply.github.com>
2023-08-07 09:23:16 +0530

    Icon,PackageName,Push Notification Done

--- App.js
+++ App.js
@@ -1,7 +1,20 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React,{useEffect} from 'react'
 import Navigation from './src/navigation/index'
+
+import {requestUserPermission, notificationListener} from './src/utiles/notificationService'
+
 const App = () => {
+
+  useEffect(() => {
+    
+    requestUserPermission()
+    notificationListener()
+     
+    }, [])
+
+
+
   return (
    <Navigation/>
   )
